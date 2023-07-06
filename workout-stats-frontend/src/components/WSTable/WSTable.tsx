47fec062ede40845b9cdeace017e0a8dfe579d6d
Ansis Strodahs <labsansis@gmail.com>
2023-07-06 02:25:25 +0100

    Tweak shading

--- workout-stats-frontend/src/components/WSTable/WSTable.tsx
+++ workout-stats-frontend/src/components/WSTable/WSTable.tsx
@@ -1,5 +1,6 @@
 import { Table, Thead, Tbody, Tr, Th, Td } from "react-super-responsive-table";
 import "react-super-responsive-table/dist/SuperResponsiveTableStyle.css";
+import "./WSTable.css";
 
 /**
  * This is a UX component styling a table.
