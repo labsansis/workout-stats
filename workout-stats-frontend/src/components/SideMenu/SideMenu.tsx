42ca2d97af71092603539b222ffb7ec8d6fbaa83
Ansis Strodahs <labsansis@gmail.com>
2023-08-05 17:09:52 +0100

    Tweak menu looks

--- workout-stats-frontend/src/components/SideMenu/SideMenu.tsx
+++ workout-stats-frontend/src/components/SideMenu/SideMenu.tsx
@@ -1,10 +1,17 @@
-import { useState } from "react";
+import { PropsWithChildren, useState } from "react";
 import "./SideMenu.css"; // CSS styles for the SideMenu component
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { userState } from "../../common/recoilStateDefs";
 import { useRecoilValue } from "recoil";
 import { FiMenu } from "react-icons/fi";
 import { RiCloseFill } from "react-icons/ri";
+import { AiOutlineHome } from "react-icons/ai";
+import { CgGym } from "react-icons/cg";
+import { MdSportsGymnastics } from "react-icons/md";
+import { GiMuscleUp } from "react-icons/gi";
+import { IconType } from "react-icons";
+import { GoUpload } from "react-icons/go";
+import { RiLogoutBoxRLine } from "react-icons/ri";
 
 const SideMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -18,6 +25,35 @@ const SideMenu = () => {
     return user?.name || user?.email;
   };
 
+  const MenuLink = ({
+    to,
+    icon,
+    children,
+  }: PropsWithChildren<MenuLinkProps>) => {
+    return (
+      <NavLink
+        to={to}
+        onClick={toggleMenu}
+        className={({ isActive, isPending }) =>
+          isActive || isPending ? "font-bold drop-shadow-md" : ""
+        }
+      >
+        {icon &&
+          icon({
+            style: {
+              display: "inline",
+              width: "1.4em",
+              height: "1.4em",
+              marginRight: "0.2em",
+              position: "relative",
+              top: "-0.1em",
+            },
+          })}{" "}
+        {children}
+      </NavLink>
+    );
+  };
+
   return (
     <div className={`side-menu ${isOpen ? "open" : ""}`}>
       <button
@@ -27,28 +63,33 @@ const SideMenu = () => {
         {isOpen ? <RiCloseFill /> : <FiMenu />}
       </button>
       <div className="menu-content">
-        <Link to="/home" onClick={toggleMenu}>
+        <MenuLink to="/home" icon={AiOutlineHome}>
           Home
-        </Link>
-        <Link to="/strength" onClick={toggleMenu}>
+        </MenuLink>
+        <MenuLink to="/strength" icon={CgGym}>
           Strength Workouts
-        </Link>
-        <Link to="/exercises" onClick={toggleMenu}>
+        </MenuLink>
+        <MenuLink to="/exercises" icon={MdSportsGymnastics}>
           Exercises
-        </Link>
-        <Link to="/volume" onClick={toggleMenu}>
+        </MenuLink>
+        <MenuLink to="/volume" icon={GiMuscleUp}>
           Training Volume
-        </Link>
-        <div className="menu-user-line bg-[#d1fae5]">
-          {getUserDisplayName()}
-        </div>
-        <Link to="/upload" onClick={toggleMenu}>
+        </MenuLink>
+        <div className="menu-user-line">{getUserDisplayName()}</div>
+        <MenuLink to="/upload" icon={GoUpload}>
           Upload files
-        </Link>
-        <Link to="/signout">Sign out</Link>
+        </MenuLink>
+        <MenuLink to="/signout" icon={RiLogoutBoxRLine}>
+          Sign out
+        </MenuLink>
       </div>
     </div>
   );
 };
 
 export default SideMenu;
+
+type MenuLinkProps = {
+  to: string;
+  icon?: IconType;
+};
