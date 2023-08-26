9c47ad3f511e515af3d5d44dbc6fe7675fff33c1
Ansis Strodahs <labsansis@gmail.com>
2023-08-26 19:52:57 +0100

    Endpoint for uploading raw workouts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -12,19 +12,62 @@ import * as logger from "firebase-functions/logger";
 import * as express from "express";
 
 import { initializeApp } from "firebase-admin/app";
-// import {getFirestore} from "firebase-admin/firestore";
+import { getFirestore } from "firebase-admin/firestore";
+import { GarminActivity } from "./models/garmin";
 
 initializeApp();
+
+const fadb = getFirestore();
 
 // REST API
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  logger.info("Hello logs!", { structuredData: true });
-  res.status(200).send("Hey there! We're now using express!");
+app.post("/rawWorkout/garmin", async (req, res) => {
+  // retrieve the upload token from the header
+  const token = req.headers["x-extension-upload-token"];
+  if (!token) {
+    res.status(401).send({ error: "Missing upload token" });
+    return;
+  }
+
+  // find the corresponding user id
+  const dbres = await fadb
+    .collectionGroup("accessTokens")
+    .where("extensionUploadToken", "==", token)
+    .get();
+  if (dbres.size !== 1) {
+    res.status(401).send({ error: "Token not valid" });
+    return;
+  }
+
+  const userId = (await dbres.docs[0].ref.parent.parent?.get())?.id;
+
+  if (!userId) {
+    res.status(401).send({ error: "Token not valid" });
+    return;
+  }
+
+  // write all raw workouts to db
+  const bw = fadb.bulkWriter();
+
+  (req.body as GarminUploadRequestBody).activities.forEach((a) => {
+    bw.set(
+      fadb
+        .collection("users")
+        .doc(userId)
+        .collection("rawWorkouts")
+        .doc(`garmin_${a.activityId}`),
+      a,
+    );
+  });
+  bw.close();
+  res.send({ status: "ok" });
 });
 
-app.post("/rawWorkout/garmin", (req, res) => {});
 exports.app = onRequest(app);
+
+type GarminUploadRequestBody = {
+  activities: GarminActivity[];
+};
