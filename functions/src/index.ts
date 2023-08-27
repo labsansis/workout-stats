be5e77903abfd7bac2651538a6e85c62e03359e3
Ansis Strodahs <labsansis@gmail.com>
2023-08-27 03:00:10 +0100

    WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,15 +9,24 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import {setGlobalOptions} from "firebase-functions/v2";
-// import * as logger from "firebase-functions/logger";
+import * as logger from "firebase-functions/logger";
 import * as express from "express";
 
 import {initializeApp} from "firebase-admin/app";
+import {credential as fbsCredential} from "firebase-admin";
 import {getFirestore} from "firebase-admin/firestore";
-import {GarminActivity} from "./models/garmin";
+// import {GarminActivity} from "./models/garmin";
 
 setGlobalOptions({maxInstances: 10});
-initializeApp();
+initializeApp({
+  credential: fbsCredential.cert({
+    projectId: "workoutstats-b571c",
+    clientEmail:
+      "",
+    privateKey:
+      "",
+  }),
+});
 
 const fadb = getFirestore();
 
@@ -25,6 +34,24 @@ const fadb = getFirestore();
 
 const app = express();
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("oki dokes");
+});
+
+app.get("/again", (req, res) => {
+  res.send("oki dokes again");
+});
+
+app.get("/create", async (req, res) => {
+  const wr = await fadb
+    .collection("dummyRecords")
+    .doc(String(Math.floor(Math.random() * 1000)))
+    .create({hello: "world"});
+  logger.info("WRITE RESULT");
+  logger.info(wr);
+  res.send("created");
+});
 
 app.post("/rawWorkout/garmin", async (req, res) => {
   // retrieve the upload token from the header
@@ -36,7 +63,7 @@ app.post("/rawWorkout/garmin", async (req, res) => {
 
   // find the corresponding user id
   const dbres = await fadb
-    .collectionGroup("accessTokens")
+    .collection("userSensitiveData")
     .where("extensionUploadToken", "==", token)
     .get();
   if (dbres.size !== 1) {
@@ -64,12 +91,12 @@ app.post("/rawWorkout/garmin", async (req, res) => {
       a,
     );
   });
-  bw.close();
+  await bw.close();
   res.send({status: "ok"});
 });
 
 exports.app = onRequest(app);
 
-type GarminUploadRequestBody = {
-  activities: GarminActivity[];
-};
+// type GarminUploadRequestBody = {
+//   activities: GarminActivity[];
+// };
